2f71745918b02bd7d9fcf730889fd4cda7bdf405
Leonardo Olivera <olezdev@gmail.com>
2023-12-14 17:23:42 -0300

    edit endpoint post shorturl

--- index.js
+++ index.js
@@ -24,39 +24,38 @@ app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req, res) => {
+app.post('/api/shorturl', (req, res, next) => {
   const { url } = req.body
   console.log(url);
 
   const hostname = new URL(url).hostname;
 
-  dns.lookup(hostname, (err, address) => {
+  dns.lookup(hostname, (err) => {
     if (err) {
-      res.status(400).json({ error: "Invalid URL" })
+      res.json({ error: "Invalid URL" });
     } else {
-
-      let data = {
-        original_url: req.body.url,
-        short_url: shortenedURL.length + 1
-      };
-      let url = new db.ShortURL({
-        original_url: req.body.url,
-        short_url: shortenedURL.length + 1
-      })
-      url.save().then(() => {
-        res.json(data);
-        console.log(`${url} saved url`);
-      }).catch((err) => {
-        res.json({ error: 'Invalid URL' });
-        console.log(`${url} error: ${err.message}`);
-      });
-
-      shortenedURL.push(data);
-
+      next();
     }
+  });
+}, (req, res) => {
+  const data = {
+    original_url: req.body.url,
+    short_url: shortenedURL.length + 1
+  };
+  let url = new db.ShortURL({
+    original_url: req.body.url,
+    short_url: shortenedURL.length + 1
   })
+  url.save().then(() => {
+    res.json(data);
+    console.log(`${data} saved`);
+  }).catch((err) => {
+    res.json({ error: 'Invalid URL' });
+    console.log(`${data} error: ${err.message}`);
+  });
+  shortenedURL.push(data);
 
-})
+});
 
 app.get('/api/shorturl/:short_url', (req, res) => {
   let short_url = parseInt(req.params.short_url)
