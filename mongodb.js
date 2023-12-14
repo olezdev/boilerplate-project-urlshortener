1b414585433540a5253a2a75e1e1373e0a77ef40
Leonardo Olivera <olezdev@gmail.com>
2023-12-14 16:47:36 -0300

    add options mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const shortURLSchema = new mongoose.Schema({
   original_url: String,
